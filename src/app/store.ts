cf5aec8db33d2838083f8e361883a74f16a1fcf5
Mohamed Zakaria <anamohamedda977@gmail.com>
2026-03-24 01:03:04 +0200

    Shopping Cart Persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,16 +3,50 @@ import authReducer from "./features/auth/authSlice";
 import { apiSlice } from "./services/api";
 import cartReducer from "./features/cart/cartSlice";
 import globalReducer from "./features/global/globalSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+const storage = {
+  getItem(key: string) {
+    return Promise.resolve(localStorage.getItem(key));
+  },
+  setItem(key: string, value: string) {
+    localStorage.setItem(key, value);
+    return Promise.resolve(value);
+  },
+  removeItem(key: string) {
+    localStorage.removeItem(key);
+    return Promise.resolve();
+  },
+};
+const persistConfig = {
+  key: "cart",
+  storage,
+};
+
+const persistedCart = persistReducer(persistConfig, cartReducer);
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    cart: cartReducer,
+    cart: persistedCart,
     global: globalReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(apiSlice.middleware),
 });
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,18 @@ import App from "./App.tsx";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { ColorModeProvider } from "./components/ui/color-mode";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import { store, persistor } from "./app/store";
 import { CookiesProvider } from "react-cookie";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <CookiesProvider>
       <ChakraProvider value={defaultSystem}>
         <ColorModeProvider>
-          <App />
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
         </ColorModeProvider>
       </ChakraProvider>
     </CookiesProvider>
