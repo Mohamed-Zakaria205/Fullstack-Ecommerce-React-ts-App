cf6a3e8ec40a97978a6f0ecc3a60e83f9029a784
Mohamed Zakaria <anamohamedda977@gmail.com>
2026-03-31 20:41:23 +0200

    onQueryStarted in RTK query

--- src/app/services/productsApi.ts
+++ src/app/services/productsApi.ts
@@ -1,5 +1,11 @@
+import type { IProduct } from "../../interfaces";
 import { apiSlice } from "./api";
 import qs from "qs";
+
+interface IUpdateRequest {
+  id: string;
+  data: IProduct;
+}
 export const productsApi = apiSlice.injectEndpoints({
   endpoints: (build) => ({
     getProducts: build.query({
@@ -40,6 +46,32 @@ export const productsApi = apiSlice.injectEndpoints({
       }),
       invalidatesTags: ["Product"],
     }),
+
+    updateProduct: build.mutation<void, IUpdateRequest>({
+      query: ({ id, data }) => ({
+        url: `/products/${id}`,
+        method: "PUT",
+        body: data,
+      }),
+
+      async onQueryStarted({ id, ...patch }, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          productsApi.util.updateQueryData(
+            "getDashboardProducts",
+            id,
+            (draft) => {
+              Object.assign(draft, patch);
+            },
+          ),
+        );
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
+      invalidatesTags: ["Product"],
+    }),
   }),
 });
 export const {
@@ -47,4 +79,5 @@ export const {
   useGetProductByIdQuery,
   useGetDashboardProductsQuery,
   useDeleteProductMutation,
+  useUpdateProductMutation,
 } = productsApi;
